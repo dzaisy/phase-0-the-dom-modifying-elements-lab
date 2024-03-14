7a6129dd8a1f4ae7781eb9db820a0f02210a6c23
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-03-14 21:28:40 +0300

    create index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const element = document.querySelector("main#main");
+element.remove();
+// no longer has DOM node 'main#main'
+
+let newHeader = document.createElement("H1");
+newHeader.setAttribute("id","victory");
+// (method) Element.setAttribute(qualifiedName: string, value: string): void
+newHeader.innerHTML = "anna is the champion"
